807561dd1b685f77e4033ba677b7fb4bd5cc4956
Mahamudul.Dev <mahamudul.dev@gmail.com>
2023-10-13 01:48:20 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# video_selling_multivendor_app
+ This is a multivendor app for selling videos
---
+++ android/app/src/main/kotlin/com/example/video_selling_multivendor_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.video_selling_multivendor_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
